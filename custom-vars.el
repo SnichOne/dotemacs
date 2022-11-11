945ce796153e8ab5abf579b4ffd1c9522c54344c
Sergey Nichiporchik <killserver@gmail.com>
2022-11-12 00:17:27 +0300

    Initial commit

---
+++ custom-vars.el
@@ -0,0 +1,30 @@
+;; ---------------------------------------------------------------------------
+;; Use Easy Customization to tweak theme.
+;; (custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ ;; )
+;; (custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ ;; '(info-menu-star ((t (:foreground "red"))))
+ ;; '(mode-line-highlight ((t (:foreground "dark orange"))))
+ ;; )
+;; ---------------------------------------------------------------------------
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(which-key dimmer modus-themes esup marginalia use-package nyan-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
