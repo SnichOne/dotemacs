daf25d218faa338e3412f2a97ded61d557aa4451
Sergey Nichiporchik <killserver@gmail.com>
2022-11-28 02:22:26 +0300

    Refactor to early-init
    It seems it is better to disable tool-bar and scroll-bar in
    early-init.el: init-time decreased by ~0.1-0.2s.

--- init.el
+++ init.el
@@ -56,20 +56,6 @@
 ;; ---------------------------------------------------------------------------
 ;; Emacs basic look.
 
-;; TODO Do not disable menu bar for the time being to see if it's useful. It's
-;; not disabled by default in the "prelude" distribution.
-;; (menu-bar-mode -1)
-
-;; Disable tool bar since it's just several buttons for the most common
-;; operations but takes too much space.
-(tool-bar-mode -1)
-
-;; Disable scroll bar to save screen space and since it hinders the ability to
-;; resize windows using mouse: it's not allowed to resize the window by clicking
-;; and dragging on the scroll bar, only clicking and dragging on the minibuffer
-;; border works (tested in Emacs 28.1).
-(scroll-bar-mode -1)
-
 ;; Highlight current line in all buffers.
 (global-hl-line-mode 1)
 
