e7d9ed25ecebadd44fc691ec2e38e1bd878052d9
Sergey Nichiporchik <killserver@gmail.com>
2023-02-19 22:42:31 +0100

    Improve LaTeX equations numbering in Org mode

--- init.el
+++ init.el
@@ -804,6 +804,52 @@
   ;; if 'org-id-link-to-org-use-id' is set.
   (require 'org-id)
 
+  ;; Improve equation numbering in LaTeX fragments in org-mode.
+  ;; By default, equation numbers, generated with latex preview in Org Mode do
+  ;; not increment above (1).
+  ;; Source: https://kitchingroup.cheme.cmu.edu/blog/2016/11/07/Better-equation-numbering-in-LaTeX-fragments-in-org-mode/
+  (defun org-renumber-environment (orig-func &rest args)
+    (let ((results '())
+          (counter -1)
+          (numberp))
+
+      (setq results (cl-loop for (begin .  env) in
+                          (org-element-map (org-element-parse-buffer) 'latex-environment
+                            (lambda (env)
+                              (cons
+                               (org-element-property :begin env)
+                               (org-element-property :value env))))
+                          collect
+                          (cond
+                           ((and (string-match "\\\\begin{equation}" env)
+                                 (not (string-match "\\\\tag{" env)))
+                            (cl-incf counter)
+                            (cons begin counter))
+                           ((string-match "\\\\begin{align}" env)
+                            (prog2
+                                (cl-incf counter)
+                                (cons begin counter)
+                              (with-temp-buffer
+                                (insert env)
+                                (goto-char (point-min))
+                                ;; \\ is used for a new line. Each one leads to a number
+                                (cl-incf counter (count-matches "\\\\$"))
+                                ;; unless there are nonumbers.
+                                (goto-char (point-min))
+                                (cl-decf counter (count-matches "\\nonumber")))))
+                           (t
+                            (cons begin nil)))))
+
+      (when (setq numberp (cdr (assoc (point) results)))
+        (setf (car args)
+              (concat
+               (format "\\setcounter{equation}{%s}\n" numberp)
+               (car args)))))
+
+    (apply orig-func args))
+
+  (advice-add 'org-create-formula-image :around #'org-renumber-environment)
+
   ;; Add by default additional LaTeX packages for Russian language support.
   ;; NOTE: I changed my mind, these settings should be better placed using
   ;; headers per file basis.
