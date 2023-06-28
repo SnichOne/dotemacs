90b33fcd9c7127738890e80c65a4624abc7ef408
Sergey Nichiporchik <killserver@gmail.com>
2023-06-28 19:09:45 +0200

    Install cape
    Also
    
    - disable instant corfu popup, it's too CPU intensive, at least for my laptop;
    - increase font size for corfu popup documentation.

---
+++ elpa/cape-20230606.904/cape-pkg.el
@@ -0,0 +1,15 @@
+(define-package "cape" "20230606.904" "Completion At Point Extensions"
+  '((emacs "27.1")
+    (compat "29.1.4.0"))
+  :commit "6c9abaf15adfbd9c6bf593609ac3bea8c8de0611" :authors
+  '(("Daniel Mendler" . "mail@daniel-mendler.de"))
+  :maintainers
+  '(("Daniel Mendler" . "mail@daniel-mendler.de"))
+  :maintainer
+  '("Daniel Mendler" . "mail@daniel-mendler.de")
+  :keywords
+  '("abbrev" "convenience" "matching" "completion" "wp")
+  :url "https://github.com/minad/cape")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
