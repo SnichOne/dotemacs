336ef97a33d662b39ec36847065f8438a6ecc70f
Sergey Nichiporchik <killserver@gmail.com>
2024-02-19 04:37:03 +0100

    Add emacs-mac specific configuration

--- early-init.el
+++ early-init.el
@@ -1,10 +1,11 @@
-;; NOTE Do not disable menu bar for the time being to see if it's useful.
+;; NOTE: Do not disable menu bar for the time being to see if it's useful.
 ;; It's not disabled by default in the "prelude" distribution.
 ;; (menu-bar-mode -1)
 
-;; Disable tool bar since it's just several buttons for the most common
-;; operations but takes too much space.
-(tool-bar-mode -1)
+;; Unless emacs-mac is used, disable tool bar since it's just several buttons
+;; for the most common operations but takes too much space.
+(unless (boundp 'mac-carbon-version-string) (tool-bar-mode -1))
+;; (tool-bar-mode -1)
 
 ;; Disable scroll bar to save screen space and since it hinders the ability
 ;; to resize windows using mouse: it's not allowed to resize the window by
